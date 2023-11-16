3c5ad185ab6288e7236c9ff5b3784efeb6e2da01
Danilo Mota Reis <101838987+DaniloMotaReis@users.noreply.github.com>
2023-11-16 16:07:50 -0300

    Initial commit

---
+++ app/src/main/java/com/example/appmaps/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.appmaps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0,0))
+                .title("Você está aqui"));
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+}
