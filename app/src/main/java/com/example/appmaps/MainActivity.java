541be705cf170cd04adab79c17556ad39d045b86
Danilo Mota Reis <101838987+DaniloMotaReis@users.noreply.github.com>
2023-11-20 17:45:01 -0300

    buttons for changing modes

--- app/src/main/java/com/example/appmaps/MainActivity.java
+++ app/src/main/java/com/example/appmaps/MainActivity.java
@@ -4,9 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -14,21 +15,51 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private GoogleMap googleMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        GoogleMapOptions options = new GoogleMapOptions();
+
+        options.mapType(GoogleMap.MAP_TYPE_NORMAL)
+                .compassEnabled(true);
+
+        SupportMapFragment mapFragment = SupportMapFragment.newInstance(options);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.map, mapFragment)
+                .commit();
         mapFragment.getMapAsync(this);
     }
 
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
+    public void onMapReady(@NonNull GoogleMap map) {
+        googleMap = map;
         googleMap.addMarker(new MarkerOptions()
                 .position(new LatLng(0,0))
                 .title("Você está aqui"));
-        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.setTrafficEnabled(true);
+    }
+
+    public void changeMapType(View view){
+        if(googleMap != null){
+
+            int id = view.getId();
+
+            if (id == R.id.btnNorm){
+                googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+            } else if (id == R.id.btnSate){
+                googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            } else if (id == R.id.btnTerr) {
+                googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+            } else if (id == R.id.btnHibr) {
+                googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            } else if (id == R.id.btnNone) {
+                googleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+            }
+        }
     }
 }
